a9c27d23856606cf76d7bb3095436d607c3543c6
Frank Machaca <58697305+piratax@users.noreply.github.com>
2022-10-13 12:11:20 -0500

    pru
    123123

--- src/main/java/com/example/demo/ProyectoFinalLpIiApplication.java
+++ src/main/java/com/example/demo/ProyectoFinalLpIiApplication.java
@@ -8,7 +8,7 @@ public class ProyectoFinalLpIiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoFinalLpIiApplication.class, args);
-            System.out.println("Pruebba");
+       
 	}
 
 }
